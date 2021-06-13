ffe240b7f213f1c58d2b6ee93fb44cd13d7b205a
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-06-13 17:44:25 +0200

    add beer to database but without blob

--- app/src/main/java/com/example/projectmobileapp/AddingBeerActivity.java
+++ app/src/main/java/com/example/projectmobileapp/AddingBeerActivity.java
@@ -2,13 +2,79 @@ package com.example.projectmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.*;
 
 public class AddingBeerActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    EditText editTextBeerName;
+    EditText editTextKindName;
+    EditText editTextPrice;
+    EditText editTextPercentOfAlcohol;
+    RatingBar ratingBarAboutBeer;
+    RatingBar ratingBarSweetness;
+    RatingBar ratingBarHopiness;
+    Button buttonAddPhoto;
+    Button buttonSaveToDatabase;
+    Intent cameraIntent;
+    ImageView imageView;
+    MyDatabaseHelper myDB;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_beer);
+
+        editTextBeerName = findViewById(R.id.editTextBeerName);
+        editTextKindName = findViewById(R.id.editTextKindName);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        editTextPercentOfAlcohol = findViewById(R.id.editTextPercentOfAlcohol);
+
+        ratingBarAboutBeer = findViewById(R.id.ratingBarAboutBeer);
+        ratingBarHopiness = findViewById(R.id.ratingBarHopiness);
+        ratingBarSweetness = findViewById(R.id.ratingBarSweetness);
+
+
+        buttonAddPhoto = findViewById(R.id.buttonAddPhoto);
+        buttonSaveToDatabase = findViewById(R.id.buttonSaveToDatabase);
+
+        imageView = findViewById(R.id.imageView);
+
+
+
+
+        buttonAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent,CAMERA_REQUEST);
+            }
+        });
+
+        buttonSaveToDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDB = new MyDatabaseHelper(AddingBeerActivity.this);
+                /*myDB.addBeer(   editTextBeerName.getText().toString().trim(),
+                                editTextKindName.getText().toString().trim(),
+                                editTextPrice.getText().toString().trim(),
+                                editTextPercentOfAlcohol.getText().toString().trim(),
+                                ratingBarAboutBeer.getRating(),
+                                ratingBarSweetness.getRating(),
+                                ratingBarHopiness.getRating(),
+                                null
+                            );*/
+                myDB.addBeer(null,null,null,null,0, 0,0,null);
+
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/projectmobileapp/MyDatabaseHelper.java
@@ -0,0 +1,79 @@
+package com.example.projectmobileapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "Beers.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "my_library";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_KIND = "kind";
+    private static final String COLUMN_PRICE = "price";
+    private static final String COLUMN_ALCOHOL = "percentOfAlcohol";
+    private static final String COLUMN_RATE_BEER = "mainRate";
+    private static final String COLUMN_HOPINESS = "hopinessRate";
+    private static final String COLUMN_SWEETNESS = "sweetnessRate";
+    private static final String COLUMN_PHOTO = "photo";
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query =
+                "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                        COLUMN_NAME + " TEXT, "+
+                        COLUMN_KIND + " TEXT, "+
+                        COLUMN_PRICE + " TEXT, "+
+                        COLUMN_ALCOHOL + " TEXT, "+
+                        COLUMN_RATE_BEER + " REAL, "+
+                        COLUMN_HOPINESS + " REAL, "+
+                        COLUMN_SWEETNESS + " REAL, "+
+                        COLUMN_PHOTO + " BLOB); ";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
+        onCreate(db);
+
+    }
+    void addBeer(String name,String kind, String price, String percentOfAlcohol, float rateAboutBeer,float hopiness, float sweetness,byte[] image)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_NAME,name);
+        cv.put(COLUMN_KIND,kind);
+        cv.put(COLUMN_PRICE,price);
+        cv.put(COLUMN_ALCOHOL,percentOfAlcohol);
+        cv.put(COLUMN_RATE_BEER,rateAboutBeer);
+        cv.put(COLUMN_HOPINESS,hopiness);
+        cv.put(COLUMN_SWEETNESS,sweetness);
+        //cv.put(COLUMN_PHOTO,image);
+
+        long result = db.insert(TABLE_NAME,null,cv);
+        if(result == -1)
+        {
+            Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(context,"Added Succesfully",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
