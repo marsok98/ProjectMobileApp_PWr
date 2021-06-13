7e4913832320cad9e3169ebb64106483f4a1c019
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-06-13 13:11:31 +0200

    layout of  activity_adding_beer.xml

---
+++ app/src/main/java/com/example/projectmobileapp/AddingBeerActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_beer);
+    }
+}
